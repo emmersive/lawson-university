6e6fb0ba279dd11fc230b8d21b7431af32e966b0
emmersive <glen@emmersivemedia.com.au>
2026-06-29 22:03:41 +1000

    Added preview script.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -84,3 +84,8 @@ function loadDelayed() {
 }
 
 loadPage();
+
+(async function loadDa() {
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+}());
